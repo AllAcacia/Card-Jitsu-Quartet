0337e0be054241681bd083ced5fed445524e8fab
AllAcacia <dsh117@uclive.ac.nz>
2025-12-22 11:15:16 +1300

    Added fn, C matrix for output, and input vectors. May change input vectors to a scalar depending if a first order measure is required.

--- source/dynamics.h
+++ source/dynamics.h
@@ -9,12 +9,18 @@ Author:   AllAcacia
 
 
 typedef struct {
-    float wn; // natural frequency
+    float fn; // natural frequency
+    float wn; // natural (angular) frequency
     float xi; // damping coefficient
-    float dt; // frequency of iteration updates
+    float dt; // interval of iteration updates
 
     Matrix2D_Float* A; // main EoM in CCF
     Matrix2D_Float* B; // input with optional CL feedback
+    Matrix2D_Float* C; // output matrix [wn**2, 0]
+    
     Matrix2D_Float* x1; // 2x1 vector of displacement and velocity (k)
     Matrix2D_Float* x2; // 2x1 vector of displacement and velocity (k+1), intended to be swapped pointrs with x1
+
+    Matrix2D_Float* u1; // 2x1 vector of input displacement and velocity (k)
+    Matrix2D_Float* u2; // 2x1 vector of input displacement and velocity (k+1), intended to be swapped pointrs with u1
 } SecondOrderDTS;

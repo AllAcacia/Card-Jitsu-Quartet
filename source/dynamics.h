3d32b018ad383fdf999fec87c9b4a0410bbaf108
AllAcacia <dsh117@uclive.ac.nz>
2025-12-21 21:37:18 +1300

    Simplified SecondOrderDTS to contain a Matrix2D_Float, variables for natural frequency and dampening, and dt to a float.

--- source/dynamics.h
+++ source/dynamics.h
@@ -9,18 +9,12 @@ Author:   AllAcacia
 
 
 typedef struct {
-    float v;  // state-space element 1
-    float dv; // state-space element 2
-    
-    float a0; // "A" matrix 0th derivative coefficient
-    float a1; // "A" matrix 1st derivative coefficient
-    
-    float kp; // proportional gain
-    float kd; // derivative gain
-    float ki; // integral gain
-
-    u64   dt; // frequency of iteration updates
+    float wn; // natural frequency
+    float xi; // damping coefficient
+    float dt; // frequency of iteration updates
 
     Matrix2D_Float* A; // main EoM in CCF
     Matrix2D_Float* B; // input with optional CL feedback
+    Matrix2D_Float* x1; // 2x1 vector of displacement and velocity (k)
+    Matrix2D_Float* x2; // 2x1 vector of displacement and velocity (k+1), intended to be swapped pointrs with x1
 } SecondOrderDTS;

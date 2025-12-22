dff9f8806be09884d82abdacc570ebe90776a076
AllAcacia <dsh117@uclive.ac.nz>
2025-12-22 17:57:07 +1300

    Made a function to initialise a state-space DTS system.

--- source/dynamics.c
+++ source/dynamics.c
@@ -5,3 +5,31 @@ Author:   AllAcacia
 
 
 #include "dynamics.h"
+
+
+SecondOrderDTS* dynamicSS_init(const float fn, const float xi, const float dt)
+{
+    SecondOrderDTS* system = malloc(sizeof(SecondOrderDTS));
+    system->fn = fn;
+    float wn = 2 * M_PI * fn;
+    system->wn = wn;
+    system->xi = xi;
+    system->dt = dt;
+
+    system->F = mat2Dfloat_init(2, 2);
+    mat2Dfloat_insert(system->F, 0, 0, 1.0f);
+    mat2Dfloat_insert(system->F, 0, 1, dt);
+    mat2Dfloat_insert(system->F, 1, 0, -(dt*wn*wn));
+    mat2Dfloat_insert(system->F, 1, 1, -(2*dt*wn*xi)+1.0f);
+
+    system->G = mat2Dfloat_init(2, 1);
+    mat2Dfloat_insert(system->G, 1, 0, 1);
+
+    system->H = mat2Dfloat_init(1, 2);
+    mat2Dfloat_insert(system->H, 0, 0, system->wn);
+
+    system->x1 = mat2Dfloat_init(2, 1); // stores the base and first order measurements of the output. Automatically 0'd.
+    system->u1 = mat2Dfloat_init(1, 1); // essentially a scalar unless later we desire first order measurement. Automatically 0'd.
+
+    return system;
+}
--- source/dynamics.h
+++ source/dynamics.h
@@ -6,6 +6,7 @@ Author:   AllAcacia
 
 #include "3ds.h" // for u64
 #include "mat2d.h"
+#include <math.h>
 
 
 typedef struct {
@@ -14,13 +15,16 @@ typedef struct {
     float xi; // damping coefficient
     float dt; // interval of iteration updates
 
-    Matrix2D_Float* A; // main EoM in CCF
-    Matrix2D_Float* B; // input with optional CL feedback
-    Matrix2D_Float* C; // output matrix [wn**2, 0]
+    Matrix2D_Float* F; // main EoM in CCF
+    Matrix2D_Float* G; // input with optional CL feedback
+    Matrix2D_Float* H; // output matrix [wn**2, 0]
     
     Matrix2D_Float* x1; // 2x1 vector of displacement and velocity (k)
-    Matrix2D_Float* x2; // 2x1 vector of displacement and velocity (k+1), intended to be swapped pointrs with x1
+    Matrix2D_Float* x2; // 2x1 vector of displacement and velocity (k+1), intended to be swapped pointers with x1
 
     Matrix2D_Float* u1; // 2x1 vector of input displacement and velocity (k)
-    Matrix2D_Float* u2; // 2x1 vector of input displacement and velocity (k+1), intended to be swapped pointrs with u1
+    Matrix2D_Float* u2; // 2x1 vector of input displacement and velocity (k+1), intended to be swapped pointers with u1
 } SecondOrderDTS;
+
+
+SecondOrderDTS* dynamicSS_init(const float fn, const float xi, const float dt);

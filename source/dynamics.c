6d30d3d079ec3e307af6c2b9e0d443fc526c0fbb
AllAcacia <dsh117@uclive.ac.nz>
2025-12-25 21:18:01 +1300

    Rewrote initializer to favour writing data to the stack instead of the heap.

--- source/dynamics.c
+++ source/dynamics.c
@@ -7,9 +7,8 @@ Author:   AllAcacia
 #include "dynamics.h"
 
 
-SecondOrderDTS* dynamicSS_init(const float fn, const float xi, const float dt)
+void dynamicSS_init(SecondOrderDTS* system, const float fn, const float xi, const float dt)
 {
-    SecondOrderDTS* system = malloc(sizeof(SecondOrderDTS));
     system->fn = fn;
     float wn = 2 * M_PI * fn;
     system->wn = wn;
@@ -17,43 +16,48 @@ SecondOrderDTS* dynamicSS_init(const float fn, const float xi, const float dt)
     system->dt = dt;
 
     system->F = mat2Dfloat_init(2, 2);
-    mat2Dfloat_insert(system->F, 0, 0, 1.0f);
-    mat2Dfloat_insert(system->F, 0, 1, dt);
-    mat2Dfloat_insert(system->F, 1, 0, -(dt*wn*wn));
-    mat2Dfloat_insert(system->F, 1, 1, -(2*dt*wn*xi)+1.0f);
+    mat2Dfloat_insert(&system->F, 0, 0, 1.0f);
+    mat2Dfloat_insert(&system->F, 0, 1, dt);
+    mat2Dfloat_insert(&system->F, 1, 0, -(dt*wn*wn));
+    mat2Dfloat_insert(&system->F, 1, 1, -(2*dt*wn*xi)+1.0f);
 
     system->G = mat2Dfloat_init(2, 1);
-    mat2Dfloat_insert(system->G, 1, 0, dt);
+    mat2Dfloat_insert(&system->G, 1, 0, dt);
 
     system->H = mat2Dfloat_init(1, 2);
-    mat2Dfloat_insert(system->H, 0, 0, system->wn);
+    mat2Dfloat_insert(&system->H, 0, 0, system->wn);
 
     system->x1 = mat2Dfloat_init(2, 1); // stores the base and first order measurements of the output. Automatically 0'd.
     system->x2 = mat2Dfloat_init(2, 1); // stores the base and first order measurements of the output. Automatically 0'd.
     system->u1 = mat2Dfloat_init(2, 1); // is the 2x1 vector, G, but linearly scaled to an input value.
-
-    return system;
 }
 
 
-void dynamicSS_del(SecondOrderDTS* system)
+// void dynamicSS_del(SecondOrderDTS* system)
+// {
+//     mat2Dfloat_del(system->F);
+//     mat2Dfloat_del(system->G);
+//     mat2Dfloat_del(system->H);
+//     mat2Dfloat_del(system->x1);
+//     mat2Dfloat_del(system->x2);
+//     mat2Dfloat_del(system->u1);
+//     free(system);
+// }
+
+
+void dynamicSS_setstate(SecondOrderDTS* system, float val1, float val2)
 {
-    mat2Dfloat_del(system->F);
-    mat2Dfloat_del(system->G);
-    mat2Dfloat_del(system->H);
-    mat2Dfloat_del(system->x1);
-    mat2Dfloat_del(system->x2);
-    mat2Dfloat_del(system->u1);
-    free(system);
+    mat2Dfloat_insert(&system->x1, 0, 0, val1);
+    mat2Dfloat_insert(&system->x1, 1, 0, val2);
 }
 
 
 void dynamicSS_iterate(SecondOrderDTS* system, const float u)
 {
     // Iterates the system.
-    mat2Dfloat_scale(system->G, system->u1, u);
-    mat2Dfloat_matmul(system->F, system->x1, system->x2);
-    mat2Dfloat_arithmetic(system->x2, system->u1, system->x1, OP_ADD);
+    mat2Dfloat_scale(&system->G, &system->u1, u);
+    mat2Dfloat_matmul(&system->F, &system->x1, &system->x2);
+    mat2Dfloat_arithmetic(&system->x2, &system->u1, &system->x1, OP_ADD);
 
-    system->y = mat2Dfloat_return(system->H, 0, 0) * mat2Dfloat_return(system->x1, 0, 0);
+    system->y = mat2Dfloat_return(&system->H, 0, 0) * mat2Dfloat_return(&system->x1, 0, 0);
 }
--- source/dynamics.h
+++ source/dynamics.h
@@ -17,18 +17,20 @@ typedef struct {
     float xi; // damping coefficient
     float dt; // interval of iteration updates
 
-    Matrix2D_Float* F; // main EoM in CCF
-    Matrix2D_Float* G; // input with optional CL feedback
-    Matrix2D_Float* H; // output matrix [wn**2, 0]
+    Matrix2D_Float F; // main EoM in CCF
+    Matrix2D_Float G; // input with optional CL feedback
+    Matrix2D_Float H; // output matrix [wn**2, 0]
 
-    Matrix2D_Float* x1; // 2x1 vector of displacement and velocity (k)
-    Matrix2D_Float* x2; // 2x1 vector of displacement and velocity (k+1), intended to be swapped pointers with x1
-    Matrix2D_Float* u1; // 2x1 vector that holds input information. Scaled version of G.
+    Matrix2D_Float x1; // 2x1 vector of displacement and velocity (k)
+    Matrix2D_Float x2; // 2x1 vector of displacement and velocity (k+1), intended to be swapped pointers with x1
+    Matrix2D_Float u1; // 2x1 vector that holds input information. Scaled version of G.
 } SecondOrderDTS;
 
 
-SecondOrderDTS* dynamicSS_init(const float fn, const float xi, const float dt);
+void dynamicSS_init(SecondOrderDTS* system, const float fn, const float xi, const float dt);
 
-void dynamicSS_del(SecondOrderDTS* system);
+// void dynamicSS_del(SecondOrderDTS* system);
+
+void dynamicSS_setstate(SecondOrderDTS* system, float val1, float val2);
 
 void dynamicSS_iterate(SecondOrderDTS* system, const float u);
